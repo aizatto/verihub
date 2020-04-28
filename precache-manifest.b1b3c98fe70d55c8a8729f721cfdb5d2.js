6ce67f416c8dc638fb9ed436e5388efe947f3a60
Aizat Faiz <aizat.faiz@gmail.com>
2020-04-28 09:47:09 +0800

    Updates

---
+++ precache-manifest.b1b3c98fe70d55c8a8729f721cfdb5d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8fb97376a63e1643881d5125c74cb0b",
+    "url": "/verihub/index.html"
+  },
+  {
+    "revision": "7b9513b15b62e66c2a0b",
+    "url": "/verihub/static/css/2.8d06fd47.chunk.css"
+  },
+  {
+    "revision": "a28c89913500bc787f91",
+    "url": "/verihub/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7b9513b15b62e66c2a0b",
+    "url": "/verihub/static/js/2.c288a6c0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/verihub/static/js/2.c288a6c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a28c89913500bc787f91",
+    "url": "/verihub/static/js/main.29f8dcef.chunk.js"
+  },
+  {
+    "revision": "b67a98a16316881b4632",
+    "url": "/verihub/static/js/runtime-main.9c6f97b8.js"
+  }
+]);
